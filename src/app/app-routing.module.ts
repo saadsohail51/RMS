b56dad6bedecae328771a44332376ccaf690f1e7
Saad Sohail <sohailsaad51@gmail.com>
2020-05-19 02:31:24 -0700

    RMS Pushed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'recruitments',
+    loadChildren: './recruitments/recruitment.module#RecruitmentModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
